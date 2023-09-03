234a79d14f626330c1aa92a72cb249f8b9f2afa7
Nabin Malla <nabin.malla@patancollege.edu.np>
2023-09-03 22:41:32 +0545

    connect database

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
+require("dotenv").config();
+
 const express = require("express");
+
+const mongoose = require("mongoose");
 const app = express();
-// const mongoose = require("mongoose");
+const indexRouter = require("./routes");
+const PORT = +process.env.PORT || 3333;
+
+mongoose.connect("mongodb://127.0.0.1:27017/todoapp").then(() => {
+  console.log("Database Connected..");
+});
+
+app.use(express.json());
+app.use("/", indexRouter);
+
+app.use((err, req, res, next) => {
+  err = err.toString() || "Something went wrong..";
+  res.status(500).json({ data: "", msg: err });
+});
 
 // const indexRouter = require("./routes");
 // const PORT = +process.env.PORT;
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("App is running on port 3333");
 });
