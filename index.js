146d34fc4fdc2106db12d8f849bc92b05aaf68ca
Nabin Malla <nabin.malla@patancollege.edu.np>
2023-09-03 15:56:16 +0545

    port

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-const indexRouter = require("./routes");
-const PORT = +process.env.PORT;
+// const indexRouter = require("./routes");
+// const PORT = +process.env.PORT;
 
-app.listen(PORT, () => {
+app.listen(3000, () => {
   console.log("App is running on port 3333");
 });
