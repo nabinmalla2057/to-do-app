5843f06e9c3a29401c8f9dcd54cb09623a15a3aa
Nabin Malla <nabin.malla@patancollege.edu.np>
2023-09-04 20:35:40 +0545

    router.get

--- modules/subtasks/subtask.api.js
+++ modules/subtasks/subtask.api.js
@@ -1,3 +1,36 @@
 const router = require("express").Router();
+const subtaskcontroller = require("./subtaskmodule");
+
+//Create
+router.post("/", async (req, res, next) => {
+  try {
+    const newSubtask = await subtaskcontroller.createSubtask(req.body);
+    res.status(201).json(newSubtask);
+  } catch (error) {
+    next(error);
+  }
+});
+
+//List
+router.get("/", async (req, res, next) => {
+  try {
+    const subtasks = await subtaskcontroller.listSubtasks();
+    res.json(subtasks);
+  } catch (error) {
+    next(error);
+  }
+});
+
+//getById
+router.get("/", async (req, res, next) => {});
+
+//updateById
+router.get("/", async (req, res, next) => {});
+
+//updateStatus
+router.get("/", async (req, res, next) => {});
+
+//removeById
+router.get("/", async (req, res, next) => {});
 
 module.exports = router;
