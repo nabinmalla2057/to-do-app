1b2ba66ebd497f0fbe768fa83f27e1180063127b
Nabin Malla <nabin.malla@patancollege.edu.np>
2023-09-07 22:32:47 +0545

    api of todo

--- modules/todos/todo.api.js
+++ modules/todos/todo.api.js
@@ -1,3 +1,42 @@
 const router = require("express").Router();
+const todoController = require("./todo.controller");
+
+router.post("/", async (req, res, next) => {
+  try {
+    const result = await todoController.create(req.body);
+    res.json({ data: result, msg: "sucessful" });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/", async (req, res, next) => {
+  try {
+    const result = await todoController.getData();
+    res.json({ data: result, msg: "Sucessful" });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.put("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await todoController.upateDataByID(id, res.body);
+    res.json({ data: result, msg: "Sucessful" });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await todoController.deleteDataByID(id);
+    res.json({ data: result, msg: "Sucessful" });
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = router;
